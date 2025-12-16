b037dc541b710b452a54a93415787fa25a895bb0
Taylor Friesen <taylor.friesen@gov.bc.ca>
2025-12-16 07:34:38 -0800

    adding day 5 and 6 work

--- day5-frequency-change/sol.py
+++ day5-frequency-change/sol.py
@@ -1,16 +1,13 @@
-"""Used as the solution for the fifth day in python"""
+"""DAY 5 """
+
 import sys
 import os
 import time
 
 def change_dir():
-    """For python debugging"""
-    os.chdir("/Users/tafriese/code/citz-imb-fsd-DQ/day5-frequency-change")
+    os.chdir("/Users/tafriese/code/FSD-AOC-SOL/citz-imb-fsd-DQ/day5-frequency-change")
 
 sys.path.append("..")
-# # pylint: disable=wrong-import-position
-# from utils import read_file
-# # pylint: enable=wrong-import-position
 
 def read_file(file_path):
     """Used to read a files content and return said content"""
@@ -19,10 +16,42 @@ def read_file(file_path):
         read_input = file.read()
     return read_input.strip()
 
+"""
+PART 1
+
+solutions:
+  - small.txt 32
+  - input.txt 508
+
+code:
+  - convert to list in main()
+  - add the following function and a call to it from main
+"""
+def parse_freq_list_part_1(in_freq: list):
+    freq = 0
+    for delta_freq in in_freq:
+        operator = delta_freq[0]
+        delta = int(delta_freq[1:])
+        #print("op: ", operator, "delta: ", delta)
+        if operator == "+":
+            freq += delta
+        elif operator == "-":
+            freq -= delta
+    return freq
+
 def main():
     """Flow of solution"""
-    input_string = read_file('./small.txt')
-    print(input_string)
+    change_dir()
+
+
+    input_from = './input.txt'
+    input_string = read_file(input_from)
+    #print(input_string)
+    input_li = input_string.split("\n")
+
+    # PART 1 SOLUTION
+    part_1_sol = parse_freq_list_part_1(input_li)
+    print(f"Part 1: with input: {input_from}\n\t{part_1_sol:^10}")
 
 if __name__ == "__main__":
     change_dir()
